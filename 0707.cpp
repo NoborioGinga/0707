7ff22166525bf33c7b8d3118e6824c41b5a1681a
4D-13\koyo <a>
2026-07-07 14:23:07 +0900

    保存

--- 0707.cpp
+++ 0707.cpp
@@ -1,25 +1,26 @@
-﻿// 0707.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
+﻿
 
 #include<iostream>
 
 class GameManager
 {
 private:
+	
+	// コンストラクタをprivateにすることで、外部からのインスタンス化を防ぐ
 	GameManager() {}
 
 
 public:
 	//コピー・代入の禁止
 
+	
 	GameManager(const GameManager&) = delete;
-
+	
 	GameManager& operator=(const GameManager&) = delete;
 
-
+	//
 	static GameManager& Instance() {
-
+		
 		static GameManager instance;  
 
 		return instance;
@@ -28,7 +29,7 @@ public:
 
 
 	void StartGame() {
-		// ゲーム開始処理
+		std::cout << "ゲームタイトル" << std::endl;
 	}
 
 
@@ -38,4 +39,6 @@ public:
 int main() {
 	//使用例
 	GameManager::Instance().StartGame();
+
+
 }
